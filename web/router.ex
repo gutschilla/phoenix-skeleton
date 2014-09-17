571df25642ec15234914d436565198292458ed2f
Martin Gutsch <gutsch@zwoelf.net>
2014-09-17 19:35:29 +0200

    testinf scop

--- web/router.ex
+++ web/router.ex
@@ -1,6 +1,10 @@
 defmodule Hello2.Router do
-  use Phoenix.Router
-
-  get "/", Hello2.PageController, :index, as: :pages
+    use Phoenix.Router
+    
+    get "/", Hello2.PageController, :index, as: :pages
+    
+    scope path: "/foo/bar" do
+        get "/:some_key", Hello2.PageController, :foo_bar
+    end
 
 end
