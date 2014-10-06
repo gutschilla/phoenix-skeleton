ebd22aca3305ff683020403d9ab63a90d930144f
Martin Gutsch <gutsch@zwoelf.net>
2014-10-06 15:54:54 +0200

    PDF module hjas ben renamed to PdfGenerator

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -25,7 +25,7 @@ defmodule Hello2.PageController do
         # most of the times, we're generating PDFs from a template
         # remember to att .html extension as there's no content type without a conn
         html = Hello2.PageView.render "index.html", %{ message: "Yehaw! A PDF!"}
-        pdf_file = PDF.generate( html )
+        pdf_file = PdfGenerator.generate( html )
         Logger.log(:info, "created PDF #{pdf_file}")
         conn = put_resp_content_type conn, "application/pdf"
         conn = send_file( conn, 200, ( pdf_file ), 0 ,  :all )
