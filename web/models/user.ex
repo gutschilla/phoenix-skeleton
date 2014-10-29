ca667b48978c3bdc431c6a7356ffea12116b4738
Martin Gutsch <gutsch@zwoelf.net>
2014-10-29 14:46:21 +0100

    Helper: Skeleton2.User.Helper.roles_of( user ) works

--- web/models/user.ex
+++ web/models/user.ex
@@ -18,4 +18,28 @@ defmodule Skeleton2.User do
         field :salt,          :string
         has_many :user_userrole_maps, Skeleton2.UserUserroleMap, foreign_key: :userrole_id
     end
+
+end
+
+defmodule Skeleton2.User.Helper do
+    import Ecto.Query
+
+    def get_query( user_id, :roles ) when is_number( user_id ) do
+        query = Skeleton2.Userrole
+        # join with map
+        |> Ecto.Query.join( :inner, [role], map in role.user_userrole_maps  )
+        # where user_id fits
+        |> Ecto.Query.where( [_role, map], map.user_id == ^user_id )
+        query
+    end
+
+    # USAGE:
+    # user  = Repo.get( Skeleton2.User, 1 )
+    # roles = Skeleton2.User.Helper.roles_of( user )
+    def roles_of( user ) do
+        get_query( user.id, :roles )
+        |> Repo.all
+        |> Enum.map( fn(userrole) -> userrole.name end )
+    end
+
 end
