115629f761d1d361821fe53bb9226b870a7073fd
gutschilla <gutsch@zwoelf.net>
2014-11-03 20:14:32 +0100

    BUGFIX: rename foreign keys to match standard

--- web/models/user_userrole_map.ex
+++ web/models/user_userrole_map.ex
@@ -7,8 +7,8 @@ defmodule Skeleton2.UserUserroleMap do
 
     # the primary_key is [user_id, userrole_id].. but ecto doesn't support that, yet
     schema "user_userrole_map", primary_key: false do
-        belongs_to :user,       Skeleton2.User,     foreign_key: :user_id
-        belongs_to :user_role,  Skeleton2.Userrole, foreign_key: :userrole_id
+        belongs_to :user,     Skeleton2.User,     foreign_key: :user_id
+        belongs_to :userrole, Skeleton2.Userrole, foreign_key: :userrole_id
         #field :user_id,     :integer
         #field :userrole_id, :integer
     end
