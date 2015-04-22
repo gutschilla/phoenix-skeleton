c2d80f0f7cded1ae21d6a6ebaa81b33945898232
gutschilla <gutsch@zwoelf.net>
2015-04-22 18:10:59 +0200

    BUGFIX: configure Plug.Static to include bower_components

--- lib/skeleton4/endpoint.ex
+++ lib/skeleton4/endpoint.ex
@@ -4,7 +4,7 @@ defmodule Skeleton4.Endpoint do
   # Serve at "/" the given assets from "priv/static" directory
   plug Plug.Static,
     at: "/", from: :skeleton4,
-    only: ~w(css images js favicon.ico robots.txt)
+    only: ~w(css images js favicon.ico bower_components robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
