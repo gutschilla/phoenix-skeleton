c5098c89ef39f18829d06889c4789d73d8765db3
Martin Gutsch <gutsch@zwoelf.net>
2015-09-08 19:43:32 +0200

    when reeasing, let brunch build a production version and run mix phoenix.digest

--- etc/compile-release.sh
+++ etc/compile-release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 MIX_ENV=prod PORT=4001 mix deps.get 
 MIX_ENV=prod PORT=4001 mix compile
+brunch build --production
+MIX_ENV=prod PORT=4001 mix phoenix.digest 
 MIX_ENV=prod PORT=4001 mix release
-
