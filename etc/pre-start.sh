ea8048af19364bb2ef186d68c8cb6a4c8831765e
Martin Gutsch <gutsch@zwoelf.net>
2015-09-08 19:45:00 +0200

    pre-start: make sure the binary is executable

--- etc/pre-start.sh
+++ etc/pre-start.sh
@@ -13,6 +13,10 @@ test -d              $ROOT/rel/$APP/tmp \
             || mkdir $ROOT/rel/$APP/tmp
 chown -R $USER:$USER $ROOT/rel/$APP/tmp
 
+# make sure the binary is executable 
+chmod ag+rx $ROOT/rel/$APP/bin/$APP
+find        $ROOT/rel/$APP/releases/ -iname $APP.sh | xargs chmod a+rx
+
 # return proper code
 exit 0
 
